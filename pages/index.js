7c768de0d77ad827f0ad6985c0a6936c631abfcb
Gavyn McKenzie <gavyn@etchapps.com>
2017-04-07 22:57:22 +0100

    feat: Install next.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Reat from 'react';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Cabin','Old Standard TT']
+  }
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>Omg</div>
+    );
+  }
+}
